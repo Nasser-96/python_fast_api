0b243694a3df29b8d7b53e64d864609057f3c358
Nasser Abunasir <79524536+Nasser-96@users.noreply.github.com>
2024-10-10 09:01:58 +0300

    update todo

--- main.py
+++ main.py
@@ -19,12 +19,12 @@ class Category(Enum):
 class Todo(BaseModel):
     title:str = Field(min_length=3)
     completed:bool
-    id:int = Field(ge=3)
+    id:int
     category:Category
 
     @field_validator("id", mode="before")
     def check_id_before(cls, value: int):
-        assert value, 'GGs' # here another validation before enter the above DTO
+        assert len(str(value)) >= 1, 'id length most be 1 or greater' # here another validation before enter the above DTO
         return value
     
     @model_validator(mode="after")
@@ -34,8 +34,8 @@ class Todo(BaseModel):
 
 
 toDos = {
-        0:Todo(title='first_title', category=Category.PERSONAL, completed=True,id=4),
-        1:Todo(title='second_title', category=Category.WORK, completed=False,id=3),
+        0:Todo(title='first_title', category=Category.PERSONAL, completed=True,id=0),
+        1:Todo(title='second_title', category=Category.WORK, completed=False,id=1),
     }
 
 
@@ -50,7 +50,7 @@ def get_todo_by_id(todo_id:int):
     return toDos[todo_id]
 
 @app.get('/todo')
-def get_completed_toDos(Hello:str, gg:str,is_completed:Optional[bool]=None) -> dict[str, list[Todo]]:
+def get_completed_toDos(Hello:str, gg:str,is_completed:Optional[bool]=None) -> dict[str, list[Todo]]: # here (gg and Hello) are query params
     if is_completed:
         filtered_toDos = [todo for todo in toDos.values() if todo.completed is is_completed ]
         return {'toDos':filtered_toDos}
@@ -64,4 +64,12 @@ def create_todo(todo:Todo) -> dict:
     if todo.id in toDos:
         raise HTTPException(status_code=400, detail=f'ID {todo.id} already exist')
     toDos[todo.id] = todo
+    return {'new_toDos': toDos}
+
+@app.put('/todo/{todo_id}')
+def update_todo(todo_id:int,todo:Todo):
+    if todo_id not in toDos:
+        raise HTTPException(status_code=400,detail=f'ID {todo_id} not exist so we can not update it')
+    toDos[todo_id] = todo
+    print(type(todo))
     return {'new_toDos': toDos}
